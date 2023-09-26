97fcb15d4e574b5b73952715ad475acf4a27d2d6
Donkemezuo <raymonddonkemezuo@pursuit.org>
2023-09-26 02:49:14 -0500

    Added base view model

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
@@ -1,5 +1,45 @@
 ﻿namespace MonkeyFinder.ViewModel;
 
-public class BaseViewModel
+// Allowing our base view model conform to the INotifyPropertyChanged wrapper to enable us observe changes 
+public class BaseViewModel : INotifyPropertyChanged
 {
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public void OnPropertyChanged([CallerMemberName] string name = null) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+    bool isBusy;
+    string title;
+
+    public bool IsBusy {
+        get => isBusy;
+        set {
+            if (isBusy == value) {
+                return;
+            } else {
+                // sets new value to isBusy variable 
+                isBusy = value;
+                OnPropertyChanged(); // Notify view of property change
+            }
+
+        }
+
+    }
+
+    public string Title {
+        get => title;
+        set {
+            if (title == value)
+            {
+                return;
+            }
+            else {
+                // sets new value to title variable 
+                title = value;
+                OnPropertyChanged(); // Notify view of property change
+            }
+
+        }
+    }
+
 }
